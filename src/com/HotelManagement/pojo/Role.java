c855af37038a00c6f4135a80c666696a01e59ae7
kuangxiaocheng-zz <66577656+kuangxiaocheng-zz@users.noreply.github.com>
2020-06-08 17:33:42 +0800

    role

---
+++ src/com/HotelManagement/pojo/Role.java
@@ -0,0 +1,8 @@
+package com.HotelManagement.pojo;
+
+public class Role {
+    private long id;
+    private  String roleName;
+    private java.sql.Timestamp creationDate;
+    private java.sql.Timestamp modifyDate;
+}
