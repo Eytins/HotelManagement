2f4d6ededa052d89893ba7a80fc7d7c5fbf7b7e2
Thousands <45256998+Thousands@users.noreply.github.com>
2020-06-10 15:42:11 +0800

    s2020年6月10日15:42:03

--- src/com/HotelManagement/controller/HotelController.java
+++ src/com/HotelManagement/controller/HotelController.java
@@ -1,12 +1,7 @@
 package com.HotelManagement.controller;
 
-import com.HotelManagement.dao.hotel.HotelMapper;
-import com.HotelManagement.dao.hotelType.HotelTypeMapper;
 import com.HotelManagement.pojo.HotelType;
 import com.HotelManagement.service.hotelType.HotelTypeService;
-import com.alibaba.fastjson.JSONArray;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,26 +10,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
 public class HotelController {
     private static final org.apache.log4j.Logger logger = Logger.getLogger(HotelController.class);
+
     @Autowired
     private HotelTypeService hotelTypeService;
 
-    @RequestMapping(value = "hotel/doUserHotelSearch", method = RequestMethod.POST)
+    @RequestMapping(value = "hotel/doUserHotelSearch" , method = RequestMethod.GET)
     @ResponseBody
-    public List<HotelType> doSerch(@RequestParam(value = "priceMin") String priceMin,
-                                   @RequestParam(value = "priceMax") String priceMax,
-                                   @RequestParam(value = "hotelName") String hotelName,
-                                   @RequestParam(value = "hotelAddress") String hotelAddress
-                             ) {
+    public List<HotelType> doSerch(@RequestParam(value = "priceMin", required = false, defaultValue = "200") String priceMin,
+                                   @RequestParam(value = "priceMax", required = false, defaultValue = "20000") String priceMax,
+                                   @RequestParam(value = "hotelName", required = false) String hotelName,
+                                   @RequestParam(value = "hotelAddress", required = false) String hotelAddress) {
         int priceMin1 = Integer.parseInt(priceMin);
         int priceMax1 = Integer.parseInt(priceMax);
-        logger.debug("------------------------------------------------------------------------");
-        return this.hotelTypeService.selectByUser(priceMin1,priceMax1,hotelName,hotelAddress);
+        return this.hotelTypeService.selectByUser(priceMin1, priceMax1, hotelName, hotelAddress);
     }
 }
