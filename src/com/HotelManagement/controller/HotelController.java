4da02bd206ce43c25f88dea1751476fdf2770022
Thousands <45256998+Thousands@users.noreply.github.com>
2020-06-10 10:52:40 +0800

    2020年6月10日09:30:02

--- src/com/HotelManagement/controller/HotelController.java
+++ src/com/HotelManagement/controller/HotelController.java
@@ -7,6 +7,7 @@ import com.HotelManagement.service.hotelType.HotelTypeService;
 import com.alibaba.fastjson.JSONArray;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,18 +21,20 @@ import java.util.List;
 
 @Controller
 public class HotelController {
-
+    private static final org.apache.log4j.Logger logger = Logger.getLogger(HotelController.class);
     @Autowired
     private HotelTypeService hotelTypeService;
 
-    @RequestMapping(value = "hotel/doUserHotelSearch", method = {RequestMethod.POST})
+    @RequestMapping(value = "hotel/doUserHotelSearch", method = RequestMethod.POST)
     @ResponseBody
-    public String doSerch(@RequestParam(value = "priceMin") Integer priceMin,
-                             @RequestParam(value = "priceMax") Integer priceMax,
-                             @RequestParam(value = "hotelName") String hotelName,
-                             @RequestParam(value = "hotelAddress") String hotelAddress
+    public List<HotelType> doSerch(@RequestParam(value = "priceMin") String priceMin,
+                                   @RequestParam(value = "priceMax") String priceMax,
+                                   @RequestParam(value = "hotelName") String hotelName,
+                                   @RequestParam(value = "hotelAddress") String hotelAddress
                              ) {
-        String json = new Gson().toJson(this.hotelTypeService.selectByUser(priceMin,priceMax,hotelName,hotelAddress));
-        return json;
+        int priceMin1 = Integer.parseInt(priceMin);
+        int priceMax1 = Integer.parseInt(priceMax);
+        logger.debug("------------------------------------------------------------------------");
+        return this.hotelTypeService.selectByUser(priceMin1,priceMax1,hotelName,hotelAddress);
     }
 }
